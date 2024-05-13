323f0fe84720e6999b0139febf8f19a5fbfbd606
nazgaret <nazgaret@gmail.com>
2024-05-13 17:23:00 +0300

    Update cmd/metadb/main.go

--- cmd/metadb/main.go
+++ cmd/metadb/main.go
@@ -142,7 +142,7 @@ func run() error {
 			}
 			defer flush()
 
-			//add pprof
+			//add pprof TODO: REMOVE IN PROD
 			runtime.SetBlockProfileRate(1)
 			go func() {
 				http.ListenAndServe("localhost:8080", nil)
