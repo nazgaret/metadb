eafa81e5105ba314a99a71a48460cc2d88120700
matvoy <matvii_plikhivskyi@epam.com>
2024-05-13 15:39:41 +0300

    add blocks to profiling

--- cmd/metadb/main.go
+++ cmd/metadb/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/metadb-project/metadb/cmd/internal/color"
 	"github.com/metadb-project/metadb/cmd/internal/common"
@@ -146,6 +147,7 @@ func run() error {
 			defer flush()
 
 			//add pprof
+			runtime.SetBlockProfileRate(1)
 			go func() {
 				http.ListenAndServe("localhost:8080", nil)
 			}()
