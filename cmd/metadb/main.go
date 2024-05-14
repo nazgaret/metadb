982eaf250280a61470b156503f6058c159e122c7
nazgaret <nazgaret@gmail.com>
2024-05-14 13:58:16 +0200

    Add the sonarcloud config and coverage

--- cmd/metadb/main.go
+++ cmd/metadb/main.go
@@ -699,7 +699,7 @@ func setupBrokerDefaultValues() (consumers, messages int, err error) {
 	if err != nil {
 		return
 	}
-	c, err := cpu.Counts(false)
+	c, err := cpu.Counts(true)
 	if err != nil {
 		return
 	}
@@ -708,5 +708,6 @@ func setupBrokerDefaultValues() (consumers, messages int, err error) {
 		consumers = 41
 		messages = 10000
 	}
+	log.Debug("CPUs count: %v; RAM: %v Gb (%v bytes);", c, gbMem, m.Total)
 	return
 }
